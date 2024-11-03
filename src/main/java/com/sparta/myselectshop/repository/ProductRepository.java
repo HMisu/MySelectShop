218ac0cee0862b4a33b316281bec58c54c00b586
Misu <misuhan33@gmail.com>
2024-11-03 12:02:44 +0900

    Implementation of Retrieving Favorite Products by Folder

--- src/main/java/com/sparta/myselectshop/repository/ProductRepository.java
+++ src/main/java/com/sparta/myselectshop/repository/ProductRepository.java
@@ -8,4 +8,9 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface ProductRepository extends JpaRepository<Product, Long> {
     Page<Product> findAllByUser(User user, Pageable pageable);
+
+    // select p.id, p.title as product_title, pf.product_id as product_id, pf.folder_id as folder_id
+    // from product p left join product_folder pf on p.id = pf.product_is
+    // where p.user_id = ? and pf.folder_id = ?;
+    Page<Product> findAllByUserAndProductFolderList_FolderId(User user, Long folderId, Pageable pageable);
 }
