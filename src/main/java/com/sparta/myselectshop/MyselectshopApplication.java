ec764471104a9edcec2a21400829c9a8f16b20e5
Misu <misuhan33@gmail.com>
2024-11-03 14:39:42 +0900

    Controller Test

--- src/main/java/com/sparta/myselectshop/MyselectshopApplication.java
+++ src/main/java/com/sparta/myselectshop/MyselectshopApplication.java
@@ -2,11 +2,10 @@ package com.sparta.myselectshop;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
 @EnableScheduling
-@EnableJpaAuditing
+// @EnableJpaAuditing 테스트 코드 수행시 방해되고 오류 발생해서 따로 config 클래스 만들어줌
 @SpringBootApplication
 public class MyselectshopApplication {
 
---
+++ src/test/java/com/sparta/myselectshop/mvc/MockSpringSecurityFilter.java
@@ -0,0 +1,30 @@
+package com.sparta.myselectshop.mvc;
+
+import jakarta.servlet.*;
+import jakarta.servlet.http.HttpServletRequest;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+
+import java.io.IOException;
+
+// Mockito처럼 가짜 필터를 만들어주어 테스트
+// 왜냐하면 기존의 시큐리티가 작동하면 테스트 수행 시 방해가 될 수 있음
+public class MockSpringSecurityFilter implements Filter {
+
+    @Override
+    public void init(FilterConfig filterConfig) {
+    }
+
+    // SecurityContextHolder : 인증 객체를 담는 공간
+    @Override
+    public void doFilter(ServletRequest req, ServletResponse res, FilterChain chain) throws IOException, ServletException {
+        SecurityContextHolder.getContext()
+                .setAuthentication((Authentication) ((HttpServletRequest) req).getUserPrincipal());
+        chain.doFilter(req, res);
+    }
+
+    @Override
+    public void destroy() {
+        SecurityContextHolder.clearContext();
+    }
+}
